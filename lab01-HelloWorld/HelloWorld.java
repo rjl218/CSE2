d01ac8aacc5388015866e9b78ba6528a66ee1b35
rjl218 <rjl218@lehigh.edu>
2015-01-23 15:44:41 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,27 @@
+//Brian Chen
+//Demo 0
+//Hello World Java Program
+//
+//first compile the program
+//      javac Helloworld.java
+// run the program
+//      java HelloWorld//
+
+// define class
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args){
+        //print the statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testest
+    }
+}
