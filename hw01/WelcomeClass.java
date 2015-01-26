bb26ac3b71dca0a4475aa4199112d30467ceca66
rjl218 <rjl218@lehigh.edu>
2015-01-26 02:18:48 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Ryan Loftus
+//hw01
+//1/25/15
+
+
+// define class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args){
+    
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-R--J--L--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This fall I released a solo album on which I played every instrument.");
+    
+    
+    
+    
+    }
+}
